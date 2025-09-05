28079888a895cc11a91a9ad04a915ed4b2cb41a0
Thiago Santos <thiagodlsantos9@gmail.com>
2025-09-05 15:00:36 -0300

    feat: add education and experience sections to curriculum

--- src/components/Curriculum/index.jsx
+++ src/components/Curriculum/index.jsx
@@ -5,22 +5,63 @@ export function Curriculum({ curriculum }) {
   return (
     <div className="curriculum-container">
       <div className="header">
-        <h1>{ curriculum.full_name }</h1>
+        <h1>{curriculum.full_name}</h1>
         <div className="contact">
           <div>
-            <Mail size={14}/>
-            { curriculum.email }
+            <Mail size={14} />
+            {curriculum.email}
           </div>
           <div>
-            <Phone size={14}/>
-            { curriculum.phone_number }
+            <Phone size={14} />
+            {curriculum.phone_number}
           </div>
           <div>
-            <MapPin size={14}/>
-            { curriculum.address }
+            <MapPin size={14} />
+            {curriculum.address}
           </div>
         </div>
-      </div>      
+      </div>
+      <div className="content">
+        <div className="education">
+          <h2>Education</h2>
+          <div className="items">
+            {curriculum.education.map((edu) => (
+              <div className="info">
+                <div className="left">
+                  <p>
+                    {edu.start_date} - {edu.end_date}
+                  </p>
+                  <p>{edu.location}</p>
+                </div>
+                <div className="right">
+                  <p className="name">{edu.school_name}</p>
+                  <p>{edu.degree}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="experience">
+
+          <h2>Experience</h2>
+          <div className="items">
+            {curriculum.experience.map((exp) => (
+              <div className="info">
+                <div className="left">
+                  <p>
+                    {exp.start_date} - {exp.end_date}
+                  </p>
+                  <p>{exp.location}</p>
+                </div>
+                <div className="right">
+                  <p className="name">{exp.company_name}</p>
+                  <p>{exp.position}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
