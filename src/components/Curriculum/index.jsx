39fbcf4fe837528a8e91742eeeaa45ca2c5f69ac
Thiago Santos <thiagodlsantos9@gmail.com>
2025-09-05 15:08:39 -0300

    fix: education and experience sections rendering when empty

--- src/components/Curriculum/index.jsx
+++ src/components/Curriculum/index.jsx
@@ -22,45 +22,48 @@ export function Curriculum({ curriculum }) {
         </div>
       </div>
       <div className="content">
-        <div className="education">
-          <h2>Education</h2>
-          <div className="items">
-            {curriculum.education.map((edu) => (
-              <div className="info">
-                <div className="left">
-                  <p>
-                    {edu.start_date} - {edu.end_date}
-                  </p>
-                  <p>{edu.location}</p>
+        {curriculum.education.length > 0 && (
+          <div className="education">
+            <h2>Education</h2>
+            <div className="items">
+              {curriculum.education.map((edu) => (
+                <div className="info">
+                  <div className="left">
+                    <p>
+                      {edu.start_date} - {edu.end_date}
+                    </p>
+                    <p>{edu.location}</p>
+                  </div>
+                  <div className="right">
+                    <p className="name">{edu.school_name}</p>
+                    <p>{edu.degree}</p>
+                  </div>
                 </div>
-                <div className="right">
-                  <p className="name">{edu.school_name}</p>
-                  <p>{edu.degree}</p>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="experience">
-
-          <h2>Experience</h2>
-          <div className="items">
-            {curriculum.experience.map((exp) => (
-              <div className="info">
-                <div className="left">
-                  <p>
-                    {exp.start_date} - {exp.end_date}
-                  </p>
-                  <p>{exp.location}</p>
+        )}
+        {curriculum.experience.length > 0 && (
+          <div className="experience">
+            <h2>Experience</h2>
+            <div className="items">
+              {curriculum.experience.map((exp) => (
+                <div className="info">
+                  <div className="left">
+                    <p>
+                      {exp.start_date} - {exp.end_date}
+                    </p>
+                    <p>{exp.location}</p>
+                  </div>
+                  <div className="right">
+                    <p className="name">{exp.company_name}</p>
+                    <p>{exp.position}</p>
+                  </div>
                 </div>
-                <div className="right">
-                  <p className="name">{exp.company_name}</p>
-                  <p>{exp.position}</p>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
